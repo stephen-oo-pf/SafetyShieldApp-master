4b30d7445589111d010804347552365c2c33b307
Stephen Oo <stephen.oo@phonefusion.com>
2020-06-22 17:59:41 -0400

    update UITimekeeper component and styles

--- src/view/dashboard/home/widget/UITimeKeeper.tsx
+++ src/view/dashboard/home/widget/UITimeKeeper.tsx
@@ -1,6 +1,6 @@
 // UITimeKeeper.tsx
 // Safety Shield App: Part of Drill Component Class
-// 6/18/2020
+// 6/22/2020
 
 import React from 'react';
 import TimeKeeper from 'react-timekeeper';
@@ -12,59 +12,83 @@ export interface IUITimeKeeperProps {
 export interface IUITimeKeeperState {
     showTime:boolean;
     time?: string;
-    hour?: number;
-    minute?: number;
+    hour?: string;
+    minute?: string;
     meridiem?: string;
 }
 export default class UITimeKeeper extends React.Component<IUITimeKeeperProps, IUITimeKeeperState> {
    
     constructor(props: IUITimeKeeperProps) {
         super(props);
+        
         this.state = {
-            showTime:true,
-            time:'12:34',
-            hour: 12,
-            minute: 34,
-            meridiem: 'pm',
+            showTime: false,
+            time:'12:00',
+            hour: '12',
+            minute: '00',
+            meridiem: 'PM',
          }  
     }
-    
-    componentDidMount() {      
+    componentDidMount() {
+
     }
+
     componentWillUnmount() {
     }
-    render() {      
+    
+    render() {  
         return (
             <div className="timekeeper">
                 {this.state.showTime &&
-                    <TimeKeeper
-                        time={this.state.time}
-                        meridiem={this.state.meridiem}
-                        onChange={ (newTime:any) => this.setState({time:newTime.formattedSimple}) }
-                        onDoneClick={ () => this.setState({showTime:false}) }
-                    />
+                    <React.Fragment>
+                        <button
+                            className="alarm-clock-plus-button"
+                            onClick={()=> this.setState({meridiem:'AM'})}
+                        >   
+                            AM
+                        </button>
+                        <button 
+                            className="alarm-clock-plus-button"
+                            onClick={()=> this.setState({meridiem:'PM'})}
+                        >
+                            PM
+                        </button>
+ 
+                        <TimeKeeper
+                            time={this.state.time}
+                            hour={this.state.hour}
+                            minute={this.state.minute}
+                            meridiem={this.state.meridiem}
+                            onChange={(newTime:any) => this.setState({time:newTime.formattedSimple})}
+                            // onDoneClick={() => this.setState({showTime:false})}
+                        />
+                        <button
+                            className="alarm-clock-plus-button"
+                            onClick={() => {
+                                return this.setState({ time: '12:00' });
+                            }}
+                        >   
+                            CANCEL
+                        </button>
+                        <button 
+                            className="alarm-clock-plus-button"
+                            onClick={()=> this.setState({showTime:false})}
+                        >
+                            OK
+                        </button>
+                    </React.Fragment>
                 }
-                
-                <br /><br />  
-                <span className="current-time">Time is now: {this.state.time} {this.state.meridiem}</span>
-                
-                <br /><br />  
-                <form className="current-time-form">
-                    <input type="text" value={this.state.hour} />
-                    <input type="text" value={this.state.minute} />
-                    <input type="text" value={this.state.meridiem} />
-               </form>        
-​
-                <br /><br />
+                <br />
                 {!this.state.showTime &&
                     <button 
                         className="alarm-clock-plus" 
                         onClick={()=> this.setState({showTime:true})}
-                        onChange={(newTime:any) => this.setState({time:newTime.formatted12})}
                     >
-                        Show Time
+                        Schedule Time
                     </button>
                 }
+                <br />
+                <span className="current-time">Time is now: {this.state.time} {this.state.meridiem}</span>
             </div>  
         );
     }
